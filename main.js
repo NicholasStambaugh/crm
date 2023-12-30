a9b9dd5b4d80cc5705179baa35008306d51d4ba8
Nick Stambaugh <nastambaugh@gmail.com>
2023-12-30 16:25:36 -0500

    config work

--- main.js
+++ main.js
@@ -12,18 +12,25 @@ let mainWindow;
 // Listen for app to be ready
 app.on('ready', function () {
   // Create new window
-  mainWindow = new BrowserWindow(
-    {
-      webPreferences: {
-        nodeIntegration: true
-      }
+  mainWindow = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    minHeight: 900,
+    maxHeight: 1080,
+    minWidth: 800,
+    maxWidth: 1920,
+    webPreferences: {
+      nodeIntegration: true
     }
-  );  // Load html in window
+  });
+
+  // Load html in window
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'mainWindow.html'),
     protocol: 'file:',
     slashes: true
   }));
+
   // Quit app when closed
   mainWindow.on('closed', function () {
     app.quit();
